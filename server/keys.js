7aea79d1d9600080710288b83868056181045845
morsathi <virendran818@gmail.com>
2020-11-27 08:52:25 +0530

    add my all file

---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://veer:74iOKkIkngS8TPuv@cluster0.dbdn7.mongodb.net/instaclone?retryWrites=true&w=majority"
+}
---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    
+    name:{
+        type:String,
+        required: true,
+        minlength: 4,
+        maxlength: 50
+    },
+   
+    email:{
+        type:String,
+        required: true,
+        minlength: 4,
+        maxlength: 50
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 200
+    }
+})
+
+mongoose.model("User",userSchema)
